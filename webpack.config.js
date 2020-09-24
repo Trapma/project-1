983f539ee19da4f35ba3eddc6bf86e3b142c0c85
trapma <trapma@yandex.ru>
2020-09-24 10:56:29 +1000

    Подключение HTMLWebpackplugin, CleanWebpackPlugin и скачивание модуля CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -10,4 +13,11 @@ module.exports = {
     filename: "[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+      new HTMLWebpackPlugin({
+          template: "./index.html"
+      }),
+      new CleanWebpackPlugin(),
+    //   new CopyPlugin()
+  ]
 };
